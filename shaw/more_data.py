9e8d6089845eaf5a825bb1ba58b1b9a214bae207
healerkx <healer.kx.yu@gmail.com>
2017-08-22 09:39:09 +0800

    load data into a DataFrame obj

--- shaw/more_data.py
+++ shaw/more_data.py
@@ -18,7 +18,7 @@ sys.path.append(project_path)
 
 import pytoml as toml
 from root import *
-from datetime import datetime
+from datetime import datetime, date
 from datetime import timedelta
 
 stock_data = StockData()
@@ -84,28 +84,32 @@ def handle_code_data(db, code):
 
 def query_data_info(db):
     cursor = db.cursor()
-
     # count how many codes
     sql = "select count(distinct(code)) from sk_stock_daily_data;"
     r = cursor.execute(sql)
     if r == 1:
         count = cursor.fetchall()
         print("Contains (%d) codes by now" % count[0][0])
+        print()
     
-    # TODO: max history date
+    # max history date
     sql = "select max(date) from sk_stock_daily_data;"
     r = cursor.execute(sql)
     date = '1970-01-01'
     if r == 1:
         date = cursor.fetchall()[0][0]
         print("The latest date is (%s)" % date)
+        print()
 
-    # TODO: count how many code date=max-history-date?
+    # count how many code date=max-history-date?
     sql = "select count(distinct(code)) from sk_stock_daily_data where date = '%s';" % date
     r = cursor.execute(sql)
     if r == 1:
         count = cursor.fetchall()
         print("Contains (%d) codes in the latest date (%s)" % (count[0][0], date))
+        print()
+
+    cursor.close()
 
 
 def insert_data(db):
@@ -118,6 +122,25 @@ def insert_data(db):
 def bank_data():
     pass
 
+def print_dataframe(db, code, filename=None):
+    sql = "select date, open, close, high, low, volume from sk_stock_daily_data where code='%s'" % code
+    with db.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
+
+        r = cursor.execute(sql)
+        if r == 0:
+            print("None data")
+            return
+        dataset = cursor.fetchall()
+
+    dates = [d['date'] for d in dataset]
+
+    import pandas as pd
+    df = pd.DataFrame(list(dataset), columns=['open', 'close', 'high', 'low', 'volume'], index=dates)
+    
+    if filename:
+        pd.to_pickle(df, filename)
+    print(df[:10])
+    return df
 
 def main(argv):
     conv = MySQLdb.converters.conversions.copy()
@@ -136,6 +159,11 @@ def main(argv):
         query_data_detail(db)     
     elif argv[0] == 'bank':
         bank_data(db)
+    else:
+        filename = None
+        if len(argv) > 1:
+            filename = argv[1]
+        print_dataframe(db, argv[0], filename)
 
 
 if __name__ == '__main__':
