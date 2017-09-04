e0b9bcaf0c21b86a7fcf588306aab74f5c4f1431
healerkx <healer.kx.yu@gmail.com>
2017-09-04 17:55:59 +0800

    Origin function supported

--- tests/func.py
+++ tests/func.py
@@ -4,6 +4,9 @@ class A:
     def f(self, a):
         print("A.f(%s)" % str(a))
 
+def f(a):
+    print("f(%s)" % str(a))
+
 # Create an object and call member function f()
 a = A()
 a.f(4)
@@ -12,6 +15,7 @@ def check(func):
     def wrapper(*args, **kwargs):
         print("decorated", end=' ')
         func(*args, **kwargs)
+    wrapper.origin_func = func
     return wrapper
 
 # Dynamically decoration the member functions
@@ -20,6 +24,15 @@ A.f = check(A.f)
 # Recall the function, this function has been decorated.
 a.f(5)
 
-b = A()
-b.f(4)
+f(6)
+f = check(f)
+f(6)
+
+f = f.origin_func
+
+f(8)
+
+f = check(f)
+f(6)
+
 
