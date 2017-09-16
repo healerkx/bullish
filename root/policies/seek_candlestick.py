7508b6bba595691ad4087d96996e3149a3473250
healerkx <healer.kx.yu@gmail.com>
2017-09-16 22:09:24 +0800

    重构, LOG中加入概率可计算标记

--- root/policies/seek_candlestick.py
+++ root/policies/seek_candlestick.py
@@ -26,7 +26,7 @@ class SeekCandlestickPolicy(Policy):
         k_data = self.load_data(args['code'])
         if k_data is None:
             return False
-        k_data = k_data[k_data.index >= datetime.strptime('2017-01-02', '%Y-%m-%d').date()]
+        k_data = k_data[k_data.index >= datetime.strptime('2014-01-01', '%Y-%m-%d').date()]
         self.dates = set(map(lambda x: str(x), k_data.index))
         self.row_iter = k_data.iterrows()
         self.ring_list = RingList(self.row_iter, 7)
@@ -70,8 +70,27 @@ class SeekCandlestickPolicy(Policy):
         result = self.seek_candle_pattern(code, context)
         return result
 
+    @staticmethod
+    def compare_func1(close1, close2):
+        return 'T' if close1 >= close2 else 'F'
+
+    @staticmethod
+    def compare_func2(close1, close2):
+        return 'T' if close1 < close2 else 'F'
+
     def get_items(self, data):
         return data['open'], data['close'], data['high'], data['low']
+
+    def get_ring_list_item(self, nth, key):
+        data = self.ring_list.nth(nth)
+        return data[1][key]
+
+    def compare_close(self, close, nth_list, compare_func):
+        result = []
+        for nth in nth_list:
+            nth_close = self.get_ring_list_item(nth, 'close')
+            result.append(compare_func(nth_close, close))
+        return result
 
     ###########################################################################
     def seek_hammer(self):
@@ -83,15 +102,10 @@ class SeekCandlestickPolicy(Policy):
         open3, close3, high3, low3 = self.get_items(data3[1])
         if (high3 - close3) / (high3 - low3) < 0.03 and (open3 - low3) / (close3 - open3) > 2:
             # Found one may be a real hammer
-            data2 = self.ring_list.nth(2)
-            data1 = self.ring_list.nth(1)
-            data0 = self.ring_list.nth(0)
-
-            data4 = self.ring_list.nth(4)
-            data5 = self.ring_list.nth(5)
-            data6 = self.ring_list.nth(6)
+            result = self.compare_close(close3, [0, 1, 2, 4, 5, 6], SeekCandlestickPolicy.compare_func1)
 
             print("#", data3[0], open3, close3, high3, low3)
+            print('*', ','.join(result))
             # TODO:
 
 
@@ -101,15 +115,10 @@ class SeekCandlestickPolicy(Policy):
         open3, close3, high3, low3 = self.get_items(data3[1])
         if (high3 - close3) / (high3 - low3) < 0.03 and (close3 - low3) / (open3 - close3) > 2:
             # Found one may be a real hanging_man
-            data2 = self.ring_list.nth(2)
-            data1 = self.ring_list.nth(1)
-            data0 = self.ring_list.nth(0)
-
-            data4 = self.ring_list.nth(4)
-            data5 = self.ring_list.nth(5)
-            data6 = self.ring_list.nth(6)
+            result = self.compare_close(close3, [0, 1, 2, 4, 5, 6], SeekCandlestickPolicy.compare_func2)
 
             print("#", data3[0], open3, close3, high3, low3)
+            print('*', ','.join(result))
 
 
     def seek_engulfing(self):
